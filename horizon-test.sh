4b10396ab6b11b744c715d6ad24c3bcf36cbf968
Andrew Beekhof <andrew@beekhof.net>
2015-03-03 13:16:06 +1100

    Implement horizon scenario

---
+++ horizon-test.sh
@@ -0,0 +1,3 @@
+
+It should be possible now to login to http://vip-horizon/dashboard with admin account.
+Note that it is still not possible to deploy instances since compute nodes are not attached.
