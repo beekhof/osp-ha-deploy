69845a47e18c6d4aeb5439405160f2ac5d2a2f9b
Fabio M. Di Nitto <fdinitto@redhat.com>
2015-05-06 15:14:28 +0200

    ceilometer: add redis and make ceilometer-central A/A
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pcmk/ceilometer-test.sh
+++ pcmk/ceilometer-test.sh
@@ -1,2 +1,4 @@
 . ${PHD_VAR_env_configdir}/keystonerc_admin
 for m in storage.objects image network volume instance ; do ceilometer sample-list -m $m | tail -2 ; done
+
+# https://bugzilla.redhat.com/show_bug.cgi?id=1127526#c2 <- for A/A testing
