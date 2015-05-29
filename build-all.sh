b1abfdf4b46bf30170a932ce927d8273688b0ecc
Fabio M. Di Nitto <fdinitto@redhat.com>
2015-05-29 09:19:01 +0200

    change compute-managed to be more flexible
    - break down compute-managed into two scenario:
      compute-managed to setup pacemaker remoted on compute nodes
      controller-managed to configure the controller plane
      (makes it easier to redeploy only one or the other)
    
    - add support for -i option to build-all to select what kind
      of compute node to deploy and now default to ha (managed)
    
    - cleanup a couple of bits in build-all while at it
      be less noisy on scenario range selection
      clear snapshot variable
      simplify definiton of compute-* node mapping
    
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- build-all.sh
+++ build-all.sh
@@ -13,9 +13,8 @@ nodeMap["virt-hosts"]="east-01 east-02 east-03 east-04"
 nodeMap["galera"]="rdo7-db1.vmnet rdo7-db2.vmnet rdo7-db3.vmnet"
 nodeMap["memcached"]="rdo7-memcache1.vmnet rdo7-memcache2.vmnet rdo7-memcache3.vmnet"
 nodeMap["swift-aco"]="rdo7-swift-brick1.vmnet rdo7-swift-brick2.vmnet rdo7-swift-brick3.vmnet"
-nodeMap["compute-common"]="east-05 east-06 east-07"
-nodeMap["compute-cluster"]="east-05 east-06 east-07"
-nodeMap["compute-managed"]="rdo7-node1.vmnet rdo7-node2.vmnet rdo7-node3.vmnet east-05 east-06 east-07"
+nodeMap["compute-nodes"]="east-05 east-06 east-07"
+nodeMap["controller-managed"]="rdo7-node1.vmnet"
 nodeMap["vmsnap"]="east-02 east-03 east-04"
 
 cluster["baremetal"]=0
@@ -28,7 +27,7 @@ variables["deployment"]="collapsed"
 variables["status"]=0
 variables["components"]="lb db rabbitmq memcache mongodb keystone glance cinder swift-brick swift neutron-server neutron-agents ceilometer heat"
 variables["scenarios-segregated"]="gateway virt-hosts vmsnap-hacks hacks vmsnap-lb lb vmsnap-galera galera vmsnap-rabbitmq rabbitmq vmsnap-memcached memcached vmsnap-mongodb mongodb vmsnap-keystone keystone vmsnap-glance glance vmsnap-cinder cinder vmsnap-swift-aco swift-aco vmsnap-swift swift vmsnap-neutron-server neutron-server vmsnap-neutron-agents neutron-agents vmsnap-nova nova vmsnap-ceilometer ceilometer vmsnap-heat heat vmsnap-horizon horizon compute-common vmsnap-compute compute-cluster"
-variables["scenarios-collapsed"]="gateway virt-hosts vmsnap-hacks hacks vmsnap-basic-cluster basic-cluster vmsnap-lb lb vmsnap-galera galera vmsnap-rabbitmq rabbitmq vmsnap-memcached memcached vmsnap-mongodb mongodb vmsnap-keystone keystone vmsnap-glance glance vmsnap-cinder cinder vmsnap-swift-aco swift-aco vmsnap-swift swift vmsnap-neutron-server neutron-server vmsnap-neutron-agents neutron-agents vmsnap-nova nova vmsnap-ceilometer ceilometer vmsnap-heat heat vmsnap-horizon horizon compute-common vmsnap-compute compute-cluster"
+variables["scenarios-collapsed"]="gateway virt-hosts vmsnap-hacks hacks vmsnap-basic-cluster basic-cluster vmsnap-lb lb vmsnap-galera galera vmsnap-rabbitmq rabbitmq vmsnap-memcached memcached vmsnap-mongodb mongodb vmsnap-keystone keystone vmsnap-glance glance vmsnap-cinder cinder vmsnap-swift-aco swift-aco vmsnap-swift swift vmsnap-neutron-server neutron-server vmsnap-neutron-agents neutron-agents vmsnap-nova nova vmsnap-ceilometer ceilometer vmsnap-heat heat vmsnap-horizon horizon compute-common vmsnap-compute"
 
 function create_phd_definition() {
     scenario=$1
@@ -83,11 +82,11 @@ while true ; do
 	    nodeMap["gateway"]="mrg-01"
 	    nodeMap["virt-hosts"]="mrg-01 mrg-02 mrg-03 mrg-04"
 	    nodeMap["vmsnap"]="mrg-02 mrg-03 mrg-04"
-	    nodeMap["compute-common"]="mrg-07 mrg-08 mrg-09"
-	    nodeMap["compute-cluster"]="mrg-07 mrg-08 mrg-09"
-	    nodeMap["compute-managed"]="rdo7-node1.vmnet rdo7-node2.vmnet rdo7-node3.vmnet mrg-07 mrg-08 mrg-09"
+	    nodeMap["compute-nodes"]="mrg-07 mrg-08 mrg-09"
+	    nodeMap["controller-managed"]="rdo7-node1.vmnet"
 	    shift;;
 	-m|--method)     redeploy=$2; shift; shift;;
+	-i|--instance)   instance=$2; shift; shift;;
 	-x) set -x ; shift;;
 	--) shift ; break ;;
 	-*) echo "unknown option: $1"; exit 1;;
@@ -125,6 +124,14 @@ if [ "x${scenarios}" = x ]; then
 	full)     scenarios="beaker baremetal $scenarios";;
 	*) echo "unknown redeploy method"; exit 1;;
     esac
+    if [ -z "$instance" ]; then
+	instance=ha
+    fi
+    case $instance in
+	ha)       scenarios="$scenarios compute-managed controller-managed";;
+	single)   scenarios="$scenarios compute-cluster";;
+        *) echo "unknown instance value"; exit 1;;
+    esac
 fi
 
 function run_phd() {
@@ -145,16 +152,15 @@ inscenario=0
 for scenario in $scenarios; do
 
     if [ -n "$fromscenario" ]; then
-        echo "Will start processing from scenario: $fromscenario (current: $scenario)"
 	if [ "$fromscenario" = "${scenario}" ]; then
 		inscenario=1
 	fi
 	if [ "$inscenario" = 0 ]; then
-		echo "Skipping $scenario"
 		continue;
 	fi
     fi
 
+    snapshot_name=""
     if [ ${variables["deployment"]} = "collapsed" ]; then
 	case $scenario in
 	    vmsnap-rollback-*)
@@ -166,7 +172,10 @@ for scenario in $scenarios; do
 		snapshot_name=$(echo $scenario | sed -e 's/vmsnap-//g')
 		scenario=vmsnap
 		;;
-	    beaker|baremetal|gateway|virt-hosts|compute-common|compute-managed|compute-cluster)
+	    compute-*)
+		nodeMap[$scenario]=${nodeMap[compute-nodes]}
+		;;
+	    beaker|baremetal|gateway|virt-hosts|controller-managed)
 		;;
 	    *) 
 		# Overwrite the node list to be the nodes of our collapsed cluster
