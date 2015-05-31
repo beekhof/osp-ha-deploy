e59aa280290e8e9fb6c168971802d33190284afa
Fabio M. Di Nitto <fdinitto@redhat.com>
2015-05-31 15:52:36 +0200

    Something broken with --location http:// download in glance. move to file
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pcmk/glance-test.sh
+++ pcmk/glance-test.sh
@@ -1,5 +1,9 @@
 . ${PHD_VAR_env_configdir}/keystonerc_admin
 
-openstack image create --container-format bare --disk-format qcow2 --public --location http://download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img cirros
+if [ ! -f ${PHD_VAR_env_configdir}/cirros-0.3.2-x86_64-disk.img ]; then
+	wget -O ${PHD_VAR_env_configdir}/cirros-0.3.2-x86_64-disk.img http://download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img
+fi
+
+openstack image create --container-format bare --disk-format qcow2 --public --file ${PHD_VAR_env_configdir}/cirros-0.3.2-x86_64-disk.img cirros
 
 openstack image list
